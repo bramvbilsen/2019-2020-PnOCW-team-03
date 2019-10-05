2dcbe9a4e4c5ebea1cf918ab5e9b5ad6432244b5
Limarizon <haloprox228@gmail.com>
2019-10-05 18:34:02 +0200

    Tried displaying image, didnt succeed. Needs work.

--- static/js/frontTest.ts
+++ static/js/frontTest.ts
@@ -2,3 +2,56 @@ console.log("ok");
 for (let i = 0; i < 100; i++) {
     console.log("MEHHHH");
 }
+
+/*$("input").change(function(e) {
+
+    
+        
+        var file = e.originalEvent.srcElement.files[0];
+        
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.setAttribute('src', reader.result as string);
+        }
+        reader.readAsDataURL(file);
+        $("input").after(img);
+
+});
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#picture-src')
+                    .attr('src', e.target.result as string)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+*/
+
+
+//displayed niet, help?
+function displayImg(input){
+	if (input.files && input.files[0]) {
+        var reader = new FileReader();
+		var path = $(input).attr('src');
+
+			
+		//input.after(img);
+		reader.onload = function(b){
+			$('#picture-src').attr('src', b.target.result as string);
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+
+$("#picture").change(function(){
+	displayImg(this);
+});
+
