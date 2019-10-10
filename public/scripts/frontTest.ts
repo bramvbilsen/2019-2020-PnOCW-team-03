bb1824aa3a8a9819c3d3c5d3acf30f6313082f99
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-10-10 15:59:49 +0200

    Added missing types

--- public/scripts/frontTest.ts
+++ public/scripts/frontTest.ts
@@ -1,44 +1,44 @@
 console.log("leven is ok.");
 
 
-function masterCamera(){
-	const player = document.getElementById("player");
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    const captureButton = document.getElementById("capture");
-    const downloadButton = document.getElementById("download");
-    var link;
+function masterCamera() {
+  const player = <HTMLVideoElement>document.getElementById("player");
+  const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const captureButton = <HTMLButtonElement>document.getElementById("capture");
+  const downloadButton = <HTMLButtonElement>document.getElementById("download");
+  var link;
 
-    const constraints = {
-      video: true
-    };
+  const constraints = {
+    video: true
+  };
 
-    captureButton.addEventListener("click", () => {
+  captureButton.addEventListener("click", () => {
     // Draw the video frame to the canvas.
     // First num par gives us the width starting point from the canvas
     // Second num par gives us the height starting point from the canvas
-		context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    context.drawImage(player, 0, 0, canvas.width, canvas.height);
     //const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    });
+  });
 
-    downloadButton.addEventListener("click", () => {
-	    canvas.toBlob(function(blob) {
-          // blob ready, download it
-        link = document.createElement("a");
-        //console.log(link);
-        link.download = "example.png";
+  downloadButton.addEventListener("click", () => {
+    canvas.toBlob(function (blob) {
+      // blob ready, download it
+      link = document.createElement("a");
+      //console.log(link);
+      link.download = "example.png";
 
-        link.href = URL.createObjectURL(blob);
-        link.click();
-          // delete the internal blob reference, to let the browser clear memory from it
-          //URL.revokeObjectURL(link.href);
-        }, "image/png");
-    });
+      link.href = URL.createObjectURL(blob);
+      link.click();
+      // delete the internal blob reference, to let the browser clear memory from it
+      //URL.revokeObjectURL(link.href);
+    }, "image/png");
+  });
 
-      // Attach the video stream to the video element and autoplay.
-    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-		player.srcObject = stream;
-    });
+  // Attach the video stream to the video element and autoplay.
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    player.srcObject = stream;
+  });
 }
 
 masterCamera();
