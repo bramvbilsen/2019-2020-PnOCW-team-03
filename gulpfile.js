328ea0b2525128f4131e2c6bcc15f30c98d9b73e
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-10-27 18:20:39 +0100

    Added time variable for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const WAITING_TIME_MS = 25000;
+
 const gulp = require("gulp");
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
@@ -12,130 +14,130 @@ const del = require("del");
 
 const tsPaths_server = ["./**/*.ts", "!./public/**/*.ts"];
 const nonTsPaths_server = [
-  "./**/*.*",
-  "!gulpfile*",
-  "!./admin/**/*",
-  "!./build/**/*",
-  "!./public/**/*",
-  "!tsconfig*",
-  "!./**/*.ts",
-  "!package-lock.json"
+	"./**/*.*",
+	"!gulpfile*",
+	"!./admin/**/*",
+	"!./build/**/*",
+	"!./public/**/*",
+	"!tsconfig*",
+	"!./**/*.ts",
+	"!package-lock.json"
 ];
 
 gulp.task("Compile server", function(done) {
-  const tsProject = ts.createProject("./tsconfig.json");
-  tsProject
-    .src()
-    .pipe(tsProject())
-    .pipe(gulp.dest(path.resolve("./build")));
-  done();
+	const tsProject = ts.createProject("./tsconfig.json");
+	tsProject
+		.src()
+		.pipe(tsProject())
+		.pipe(gulp.dest(path.resolve("./build")));
+	done();
 });
 
 gulp.task("Watch server TS", function() {
-  return gulp.watch(tsPaths_server, gulp.series("Compile server"));
+	return gulp.watch(tsPaths_server, gulp.series("Compile server"));
 });
 
 gulp.task("Copy server non-TS", function(done) {
-  gulp
-    .src(nonTsPaths_server, { base: "." })
-    .pipe(gulp.dest(path.resolve("./build")));
-  done();
+	gulp.src(nonTsPaths_server, { base: "." }).pipe(
+		gulp.dest(path.resolve("./build"))
+	);
+	done();
 });
 
 gulp.task("Watch server non-TS", function() {
-  return gulp.watch(nonTsPaths_server, gulp.series("Copy server non-TS"));
+	return gulp.watch(nonTsPaths_server, gulp.series("Copy server non-TS"));
 });
 
 gulp.task(
-  "Watch server",
-  gulp.parallel(
-    gulp.series("Compile server", "Watch server TS"),
-    gulp.series("Copy server non-TS", "Watch server non-TS")
-  )
+	"Watch server",
+	gulp.parallel(
+		gulp.series("Compile server", "Watch server TS"),
+		gulp.series("Copy server non-TS", "Watch server non-TS")
+	)
 );
 
 // ------ CLIENT ------ //
 
 const tsPaths_client = ["./public/**/*.ts"];
 const nonTsPaths_client = [
-  "./public/**/*.*",
-  "!./public/build/**/*.*",
-  "!./public/tsconfig*",
-  "!./**/*.ts",
-  "!./public/package-lock.json"
+	"./public/**/*.*",
+	"!./public/build/**/*.*",
+	"!./public/tsconfig*",
+	"!./**/*.ts",
+	"!./public/package-lock.json"
 ];
 
 gulp.task("Compile client", function() {
-  // const tsProject = ts.createProject("./public/tsconfig.json");
-  // tsProject
-  // 	.src()
-  // 	.pipe(tsProject())
-  // 	.pipe(gulp.dest(path.resolve("./build/public")));
-  // done();
+	// const tsProject = ts.createProject("./public/tsconfig.json");
+	// tsProject
+	// 	.src()
+	// 	.pipe(tsProject())
+	// 	.pipe(gulp.dest(path.resolve("./build/public")));
+	// done();
 
-  return browserify({
-    basedir: path.resolve("./public/"),
-    debug: true,
-    entries: ["index.ts"],
-    // transform: ["babelify"],
-    cache: {},
-    packageCache: {}
-  })
-    .plugin(tsify, require("./public/tsconfig.json").compilerOptions)
-    .transform(babelify.configure({ extensions: [".ts", ".js"] }))
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest(path.resolve("./build/public")));
+	return browserify({
+		basedir: path.resolve("./public/"),
+		debug: true,
+		entries: ["index.ts"],
+		// transform: ["babelify"],
+		cache: {},
+		packageCache: {}
+	})
+		.plugin(tsify, require("./public/tsconfig.json").compilerOptions)
+		.transform(babelify.configure({ extensions: [".ts", ".js"] }))
+		.bundle()
+		.pipe(source("bundle.js"))
+		.pipe(gulp.dest(path.resolve("./build/public")));
 });
 
 gulp.task("Watch client TS", function() {
-  return gulp.watch(tsPaths_client, gulp.series("Compile client"));
+	return gulp.watch(tsPaths_client, gulp.series("Compile client"));
 });
 
 gulp.task("Copy client non-TS", function(done) {
-  gulp
-    .src(nonTsPaths_client, { base: "./public" })
-    .pipe(gulp.dest(path.resolve("./build/public")));
-  done();
+	gulp.src(nonTsPaths_client, { base: "./public" }).pipe(
+		gulp.dest(path.resolve("./build/public"))
+	);
+	done();
 });
 
 gulp.task("Watch client non-TS", function() {
-  return gulp.watch(nonTsPaths_client, gulp.series("Copy client non-TS"));
+	return gulp.watch(nonTsPaths_client, gulp.series("Copy client non-TS"));
 });
 
 gulp.task(
-  "Watch client",
-  gulp.parallel(
-    gulp.series("Compile client", "Watch client TS"),
-    gulp.series("Copy client non-TS", "Watch client non-TS")
-  )
+	"Watch client",
+	gulp.parallel(
+		gulp.series("Compile client", "Watch client TS"),
+		gulp.series("Copy client non-TS", "Watch client non-TS")
+	)
 );
 
 // ------ SHARED ------ //
 
 gulp.task("Clean", function(done) {
-  del.sync(["./build", "./public/build"]);
-  done();
+	del.sync(["./build", "./public/build"]);
+	done();
 });
 
 gulp.task(
-  "build:production",
-  gulp.series(
-    "Compile server",
-    "Copy server non-TS",
-    "Compile client",
-    "Copy client non-TS"
-  )
+	"build:production",
+	gulp.series(
+		"Compile server",
+		"Copy server non-TS",
+		"Compile client",
+		"Copy client non-TS"
+	)
 );
 
 gulp.task("Open localhost", function(done) {
-  setTimeout(() => {
-    nodemon({
-      script: "./build/index.js",
-      ext: "js",
-      done: done
-    });
-  }, 10000);
+	setTimeout(() => {
+		nodemon({
+			script: "./build/index.js",
+			ext: "js",
+			done: done
+		});
+	}, WAITING_TIME_MS);
 });
 
 // ------ DEFAULT ------ //
@@ -146,9 +148,9 @@ gulp.task("Open localhost", function(done) {
 ));*/
 
 gulp.task(
-  "default",
-  gulp.series(
-    "Clean",
-    gulp.parallel("Watch client", "Watch server", "Open localhost")
-  )
+	"default",
+	gulp.series(
+		"Clean",
+		gulp.parallel("Watch client", "Watch server", "Open localhost")
+	)
 );
