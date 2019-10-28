38dda00c78bb59201b94ef3c9157d5db5e2671be
Limarizon <haloprox228@gmail.com>
2019-10-28 17:51:08 +0100

    kleine bufixes client

--- public/scripts/frontTest.ts
+++ public/scripts/frontTest.ts
@@ -11,7 +11,7 @@ export default function masterCamera() {
   };
 
   captureButton.click(() => {
-    $('#camera').replaceWith($('#canvas'));
+    //$('#camera').replaceWith($('#canvas'));
     context.drawImage(player[0], 0, 0, canvas[0].width, canvas[0].height);
     // $('#capture').replaceWith('<button id="upload" class="button2" style="vertical-align:middle"><span>Upload </span></button>');
   });
