406e159b9a3139c7bf27998bedaf691abb5e7174
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-10-31 16:46:58 +0100

    fixed import

--- public/scripts/frontTest.ts
+++ public/scripts/frontTest.ts
@@ -1,6 +1,6 @@
 import findScreen from "./image_processing/screen_detection/screen_detection";
-import {IHSLColor, IRGBAColor} from "./types/Color"
-import client from '../index'
+import { IHSLColor, IRGBAColor } from "./types/Color"
+import { client } from '../index'
 export default function masterCamera() {
   const player: JQuery<HTMLVideoElement> = $("#player");
   const canvas: JQuery<HTMLCanvasElement> = $("#canvas");
@@ -12,7 +12,7 @@ export default function masterCamera() {
   const green: JQuery<HTMLButtonElement> = $("#green");
   const blue: JQuery<HTMLButtonElement> = $("#blue");
   const orange: JQuery<HTMLButtonElement> = $("#orange");
-  var rgb = 1 ;
+  var rgb = 1;
   const constraints = {
     video: true
   };
@@ -33,33 +33,33 @@ export default function masterCamera() {
   });
 
 
-  mobileCaptureButton.click(() =>{
+  mobileCaptureButton.click(() => {
     //document.getElementById('file-input').click();
 
-    document.getElementById('file-input').onchange = function(e : any = HTMLInputElement) {
+    document.getElementById('file-input').onchange = function (e: any = HTMLInputElement) {
       console.log('i got in');
       let files: any = e.target.files[0], file;
       if (files && files.length > 0) {
         file = files[0];
-        processImg({file: file});
+        processImg({ file: file });
       }
     }
   });
 
-  pink.click(()=>{
-    client.color = {r: 255, g:70, b:181,a:100}
+  pink.click(() => {
+    client.color = { r: 255, g: 70, b: 181, a: 100 }
   });
 
-  green.click(()=>{
-    client.color = {r: 0, g:128, b:0,a:100}
+  green.click(() => {
+    client.color = { r: 0, g: 128, b: 0, a: 100 }
   });
 
-  orange.click(()=>{
-    client.color = {r: 255, g:69, b:0,a:100}
+  orange.click(() => {
+    client.color = { r: 255, g: 69, b: 0, a: 100 }
   });
 
-  blue.click(()=>{
-    client.color = {r: 0, g:255, b:0,a:100}
+  blue.click(() => {
+    client.color = { r: 0, g: 255, b: 0, a: 100 }
   });
 
   // Attach the video stream to the video element and autoplay.
@@ -68,15 +68,15 @@ export default function masterCamera() {
   });
 }
 
-function processImg({file}: { file: any }) {
+function processImg({ file }: { file: any }) {
   var reader = new FileReader();
   reader.readAsDataURL(file)
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     var img = new Image();
-    img.onload = function() {
-      let canvas : JQuery<HTMLCanvasElement>= $("#canvas");;
+    img.onload = function () {
+      let canvas: JQuery<HTMLCanvasElement> = $("#canvas");;
       let context = canvas[0].getContext('2d');;
-      context.drawImage(img, 0,0, canvas[0].width, canvas[0].height)
+      context.drawImage(img, 0, 0, canvas[0].width, canvas[0].height)
     }
     if (typeof reader.result === "string") {
       img.setAttribute('src', reader.result);
