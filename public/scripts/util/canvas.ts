cc82c6caad5beba069a95c3addd70b3f12f8dc29
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-01 18:52:29 +0100

    Fixed canvas error

--- public/scripts/util/canvas.ts
+++ public/scripts/util/canvas.ts
@@ -3,6 +3,6 @@ import { createCanvas } from "../image_processing/screen_detection/screen_detect
 export function copyCanvas(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext("2d");
     const cv = createCanvas(canvas.width, canvas.height);
-    cv.getContext("2d").putImageData(ctx.getImageData(0, 0, canvas[0].width, canvas[0].height), 0, 0);
+    cv.getContext("2d").putImageData(ctx.getImageData(0, 0, canvas.width, canvas.height), 0, 0);
     return cv;
 }
