195813fe127a6129b8b6c6a06ec746fe8c2879ab
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-04 17:37:28 +0100

    Fixed imports

--- public/scripts/image_processing/orientation_detection/orientation_detection_test.ts
+++ public/scripts/image_processing/orientation_detection/orientation_detection_test.ts
@@ -1,12 +1,26 @@
 import Point from "../screen_detection/Point";
-import {getAllCentroids, cornerLabeling, getAngle} from "./orientation_detection";
+import {
+    getAllCentroids,
+    cornerLabeling,
+    getAngle
+} from "./orientation_detection";
 
-const POINTS = [new Point(341,283), new Point(650,280),new Point(646,498),new Point(350,506)];
-
+const POINTS = [
+    new Point(341, 283),
+    new Point(650, 280),
+    new Point(646, 498),
+    new Point(350, 506)
+];
 
 function runAllTests() {
-	labelingTest1(cornerLabeling, new Point(0,0), new Point(2,2), new Point(2,0), new Point(0,2));
-	/*labelingTest2(cornerLabeling, [80,120], [120,80], [120, 120], [80,80]);
+    labelingTest1(
+        cornerLabeling,
+        new Point(0, 0),
+        new Point(2, 2),
+        new Point(2, 0),
+        new Point(0, 2)
+    );
+    /*labelingTest2(cornerLabeling, [80,120], [120,80], [120, 120], [80,80]);
 	labelingTest3(cornerLabeling, [5,7], [7,2], [12,5], [10,9]);
 
 	angleSpeedTest(getAngle, [0,0], [2,2], [2,0], [0,2]);
@@ -25,50 +39,103 @@ function runAllTests() {
     */
 }
 
-
-
 /* 1) CENTROID-TESTS */
 function getAllCentroidsTest() {
     console.log(getAllCentroids(POINTS)["0"].x == 420);
     console.log(getAllCentroids(POINTS)["0"].y == 338);
-    
+
     console.log(getAllCentroids(POINTS)["1"].x == 573);
     console.log(getAllCentroids(POINTS)["1"].y == 336);
-    
+
     console.log(getAllCentroids(POINTS)["2"].x == 572);
     console.log(getAllCentroids(POINTS)["2"].y == 445);
-    
+
     console.log(getAllCentroids(POINTS)["3"].x == 423);
     console.log(getAllCentroids(POINTS)["3"].y == 449);
 }
 
 /* 2) LABELING-TESTS */
-function labelingTest1(fct: (p1: Point, p2: Point, p3: Point, p4: Point) => {[key: string]: Point}, p1: Point, p2: Point, p3: Point, p4: Point){
+function labelingTest1(
+    fct: (
+        p1: Point,
+        p2: Point,
+        p3: Point,
+        p4: Point
+    ) => { [key: string]: Point },
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    p4: Point
+) {
     var expectedResult = [p1, p3, p2, p4];
     var result = fct(p1, p2, p3, p4);
-    var correct = (result[0] == expectedResult[0] && result[1] == expectedResult[1] && result[2] == expectedResult[2] && result[3] == expectedResult[3]);
+    var correct =
+        result[0] == expectedResult[0] &&
+        result[1] == expectedResult[1] &&
+        result[2] == expectedResult[2] &&
+        result[3] == expectedResult[3];
     console.log(correct);
 }
 
-function labelingTest2(fct: (p1: Point, p2: Point, p3: Point, p4: Point) => {[key: string]: Point}, p1: Point, p2: Point, p3: Point, p4: Point){
+function labelingTest2(
+    fct: (
+        p1: Point,
+        p2: Point,
+        p3: Point,
+        p4: Point
+    ) => { [key: string]: Point },
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    p4: Point
+) {
     var expectedResult = [p4, p2, p3, p1];
     var result = fct(p1, p2, p3, p4);
-    var correct = (result[0] == expectedResult[0] && result[1] == expectedResult[1] && result[2] == expectedResult[2] && result[3] == expectedResult[3]);
+    var correct =
+        result[0] == expectedResult[0] &&
+        result[1] == expectedResult[1] &&
+        result[2] == expectedResult[2] &&
+        result[3] == expectedResult[3];
     console.log(correct);
 }
 
-function labelingTest3(fct: (p1: Point, p2: Point, p3: Point, p4: Point) => {[key: string]: Point}, p1: Point, p2: Point, p3: Point, p4: Point){
+function labelingTest3(
+    fct: (
+        p1: Point,
+        p2: Point,
+        p3: Point,
+        p4: Point
+    ) => { [key: string]: Point },
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    p4: Point
+) {
     var expectedResult = [p2, p3, p4, p1];
     var result = fct(p1, p2, p3, p4);
-    var correct = (result[0] == expectedResult[0] && result[1] == expectedResult[1] && result[2] == expectedResult[2] && result[3] == expectedResult[3]);
+    var correct =
+        result[0] == expectedResult[0] &&
+        result[1] == expectedResult[1] &&
+        result[2] == expectedResult[2] &&
+        result[3] == expectedResult[3];
     console.log(correct);
 }
 
 /* 3) ANGLE-TESTS */
-function angleSpeedTest(fct: (p1: Point, p2: Point, p3: Point, p4: Point) => number, p1: Point, p2: Point, p3: Point, p4: Point) {
+function angleSpeedTest(
+    fct: (p1: Point, p2: Point, p3: Point, p4: Point) => number,
+    p1: Point,
+    p2: Point,
+    p3: Point,
+    p4: Point
+) {
     var start = new Date();
     fct(p1, p2, p3, p4);
     var end = new Date();
     var time = +end - +start;
-    console.log("The algorithm to calculate the Angle took ", time, "milliseconds")
+    console.log(
+        "The algorithm to calculate the Angle took ",
+        time,
+        "milliseconds"
+    );
 }
