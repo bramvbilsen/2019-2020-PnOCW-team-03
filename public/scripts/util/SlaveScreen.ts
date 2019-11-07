aba5a17b1f6e63e596d3a7fad735a8a5dc2b8026
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-07 15:57:37 +0100

    Added centroid function in screen class

--- public/scripts/util/SlaveScreen.ts
+++ public/scripts/util/SlaveScreen.ts
@@ -8,4 +8,17 @@ export default class SlaveScreen {
         this.corners = corners;
         this.slaveID = slaveID;
     }
+
+    get centroid(): Point {
+        var sumX = 0;
+        var sumY = 0;
+        this.corners.forEach(point => {
+            sumX += point.x;
+            sumY += point.y;
+        });
+        return new Point(
+            Math.round(sumX / this.corners.length),
+            Math.round(sumY / this.corners.length)
+        );
+    }
 }
