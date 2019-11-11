8bfbb3b888d2859e7d18b596b575edcebaa6e002
MaartenTaylor <36727362+MaartenTaylor@users.noreply.github.com>
2019-11-11 01:06:38 +0100

    Fix bug Dalauney algorithm

--- public/scripts/image_processing/Triangulation/Delaunay.ts
+++ public/scripts/image_processing/Triangulation/Delaunay.ts
@@ -4,10 +4,20 @@ import Triangulation from "./Triangulation";
 import Circle from "./Circle";
 var gauss = require("gaussian-elimination");
 
+//punten moeten geordend worden volgens dit
+//cd.sort(function(a,b) {
+//  if (a.x-b.x == 0) {
+//    return a.y-b.y
+//  }
+//  else {
+//    return a.x-b.x
+// }
+//});
+
 function delauney(points: Point[]): Triangulation {
     //basisgevallen, geven triangulaties terug
     if (points.length == 3) {
-          let line1 = new Line(points[0],points[1]);
+      let line1 = new Line(points[0],points[1]);
       let line2 = new Line(points[0],points[2]);
       let line3 = new Line(points[2],points[1]);
       let trian: Triangulation = new Triangulation([line1,line2,line3]);
@@ -31,13 +41,12 @@ function delauney(points: Point[]): Triangulation {
     let new_width = Math.ceil(points.length/2);
     var left = points.slice(0,new_width);
     var right = points.slice(new_width,points.length);
+    console.log(left);
+    console.log(right);
     return delauneyMerging(delauney(left),delauney(right));
   }
 
 function delauneyMerging(left: Triangulation, right: Triangulation) {
-  console.log('---------------');
-  console.log(left.lines);
-  console.log(right.lines);
   if (left.lines.length == 0 || right.lines.length == 0) {
     let merge = new Triangulation(left.lines.concat(right.lines));
     return merge;
@@ -48,14 +57,8 @@ function delauneyMerging(left: Triangulation, right: Triangulation) {
   let merged = false;
 
   while (!merged) {
-    console.log('=================');
-    console.log(base);
-
-    //console.log(base);
     let leftPotential = potential(left,base,0);
     let rightPotential = potential(right,base,1);
-    console.log(leftPotential);
-    console.log(rightPotential);
 
     if (leftPotential == null && !(rightPotential == null)) {
       lr.push(new Line(base.endPoints[0],rightPotential));
@@ -135,23 +138,14 @@ function findbase(left: Triangulation, right: Triangulation) {
 }
 
 function potential(triang: Triangulation,base: Line, index: number) {
-    console.log('!!!!!');
-    console.log(triang.lines);
     let lines = triang.linesWithCertainPoints(base.endPoints[index]);
-    console.log(lines);
-    //console.log(triang.lines);
-    //console.log(base.endPoints[index]);
-    //console.log(lines);
     lines = lines.filter(function(value) {
       return base.lineAbove(value,index);
     });
     lines.sort(function(a,b) {
-      return base.angle(a,index) - base.angle(b,index); //efficienter om met dictionaries te werken wss
+      return base.angle(a,index) - base.angle(b,index); 
     });
-    console.log(lines);
-    //console.log(lines);
     for (let i = 0; i < lines.length; i++) {
-      console.log(lines[i]);
       let points = lines[i].endPoints;
       let line3;
       let potenPoint;
@@ -169,23 +163,13 @@ function potential(triang: Triangulation,base: Line, index: number) {
           return potenPoint;
         }
         else {
-          console.log('YYYYYYYYYYY');
-          console.log(base.angle(lines[i],index));
-          console.log(potenPoint);
-          triang.remove(lines[i]);
           return null;
         }
       }
-      else if (base.angle(lines[i],index) < 180 && !lines[i].surroundingCircle(base,line3).liesInCircle(lines[i+1],base.endPoints[index])) { //efficienter!
-        console.log(lines[i].surroundingCircle(base,line3));
-        console.log(lines[i+1]);
+      else if (base.angle(lines[i],index) < 180 && !lines[i].surroundingCircle(base,line3).liesInCircle(lines[i+1],base.endPoints[index])) { 
         return potenPoint;
       }
       else {
-        console.log('hhhhh');
-        console.log('YYYYYYYYYYY');
-        console.log(lines[i].surroundingCircle(base,line3));
-        console.log(potenPoint);
         triang.remove(lines[i]);
       }
     }
@@ -203,17 +187,3 @@ function decide(right: Point, left:Point ,base:Line) {
     }
   }
 
-var a = new Point(0,1);  
-var b = new Point(1,0);  
-var c = new Point(2,1);  
-var d = new Point(1,2);  
-var e = new Point(1,3);  
-var f = new Point(3,3);  
-var g = new Point(4,2);  
-var h = new Point(5,3);  
-var i = new Point(5,1);  
-var j = new Point(5,0);  
-
-var tr = delauney([a,b,c,d,e,f,g,h,i,j]);
-console.log(tr.lines);
-console.log(tr.lines.length);
