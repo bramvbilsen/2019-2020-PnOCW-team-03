22c8b71ef88852f875932b6b0a323e442df90c47
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-15 22:33:06 +0100

    Added method to scale and cut image to display on slaves

--- public/scripts/util/images.ts
+++ public/scripts/util/images.ts
@@ -1,5 +1,16 @@
-export function cutImageToBoundingBoxAspectRatio(
-    image: HTMLImageElement,
-    boundingBoxWidth: number,
-    boundingBoxHeight: number
-) {}
+import { BoudingBoxOfSlaveScreens } from "./BoundingBox";
+import { createCanvas } from "../image_processing/screen_detection/screen_detection";
+
+export function scaleAndCutImageToBoundingBoxAspectRatio(
+    img: HTMLCanvasElement,
+    boundingBox: BoudingBoxOfSlaveScreens
+): HTMLCanvasElement {
+    const scale = Math.max(
+        boundingBox.width / img.width,
+        boundingBox.height / img.height
+    );
+    const canvas = createCanvas(boundingBox.width, boundingBox.height);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+    return canvas;
+}
