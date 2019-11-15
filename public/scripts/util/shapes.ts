f82805f91f0cc05e8529e17b79380a16ca622446
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-15 21:15:14 +0100

    Added extremely simple bounding box algo

--- public/scripts/util/shapes.ts
+++ public/scripts/util/shapes.ts
@@ -12,3 +12,20 @@ export function getCentroidOf(points: Point[]): Point {
         Math.round(sumY / points.length)
     );
 }
+
+export function boundingBox(
+    points: Point[]
+): { topLeft: Point; topRight: Point; bottomLeft: Point; bottomRight: Point } {
+    const xCoordinates = points.map(point => point.x).sort((a, b) => a - b);
+    const yCoordinates = points.map(point => point.y).sort((a, b) => a - b);
+    const minX = xCoordinates[0];
+    const maxX = xCoordinates[xCoordinates.length - 1];
+    const minY = yCoordinates[0];
+    const maxY = yCoordinates[yCoordinates.length - 1];
+    return {
+        topLeft: new Point(minX, minY),
+        bottomRight: new Point(maxX, maxY),
+        topRight: new Point(maxX, minY),
+        bottomLeft: new Point(minX, maxY),
+    };
+}
