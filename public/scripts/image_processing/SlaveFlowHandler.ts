47557beaad63abab0891bb051c347c43e22d6a0c
Bram Vanbilsen <bramvbilsen@gmail.com>
2019-11-18 01:13:09 +0100

    Fixed angle a bit

--- public/scripts/image_processing/SlaveFlowHandler.ts
+++ public/scripts/image_processing/SlaveFlowHandler.ts
@@ -207,12 +207,6 @@ export default class SlaveFlowHandler {
             currScreen,
             orientationCanvas
         );
-        console.log("ORIENTATION: " + currScreen.orientation);
-        currScreen.orientation =
-            calculateOrientation(currScreen, orientationCanvas) +
-            currScreen.widthEdge.angleBetweenEndpoints -
-            180;
-        console.log("ANGLE: " + currScreen.orientation);
         client.toggleOrientationColorsOnSlave(this.currSlaveID);
         this.endSlaveCycle();
     }
