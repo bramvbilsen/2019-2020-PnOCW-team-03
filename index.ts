1f61041f1a66d41f6f75ca0e51b60588b50f8220
MaartenTaylor <36727362+MaartenTaylor@users.noreply.github.com>
2019-11-21 19:04:17 +0100

    showing triangulation fixed

--- index.ts
+++ index.ts
@@ -155,16 +155,17 @@ io.on("connect", (socket: socketio.Socket) => {
         }
     });
 
-    socket.on(MasterEventTypes.SendTriangulationOnSlave, ( msg:{
-        slaveId: string,
-        centroid: {x: number, y: number},
-        lines: any,
-    }   
-    ) => {
-        if (socket.id === connections.master.id) {
-            io.to(msg.slaveId).emit(SlaveEventTypes.DisplayTriangulationOnSlave,msg)
+    socket.on(
+        MasterEventTypes.SendTriangulationOnSlave,
+        (msg: { slaveId: string; angles: any }) => {
+            if (socket.id === connections.master.id) {
+                io.to(msg.slaveId).emit(
+                    SlaveEventTypes.DisplayTriangulationOnSlave,
+                    msg
+                );
+            }
         }
-    });
+    );
 });
 
 server.listen(port, () => {
