76cb8f3d0a81b50ccc845aa2451e410df9b3bb6f
SebastiaanWouters <sebastiaan.wouters@student.kuleuven.be>
2020-02-18 14:20:17 +0100

    colorTesting modifief to not cause errors

--- public/tests/color_detection/colorTesting.ts
+++ public/tests/color_detection/colorTesting.ts
@@ -1,11 +1,12 @@
-//TODO: import createCanvas
+import { createCanvas } from "../../scripts/image_processing/screen_detection/screen_detection";
+
 
 /**
  * 
  * @param src The path to the image that gets loaded and from which we want to analyze te correctness of the colors
  */
-
- //function for loading in images
+/*
+//function for loading in images
 export async function loadImage(src: string): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -18,29 +19,20 @@ export async function loadImage(src: string): Promise<HTMLImageElement> {
         };
     });
 }
-
 //declare a variable 
-
 const image = await loadImage(
         //"http://localhost:3000/images/unicorn.jpeg");
-
 //create a canvas with which we can iterate the pixels
 const imgCanvas = createCanvas(image.width, image.height);
 const imgCtx = imgCanvas.getContext("2d");
 imgCtx.drawImage(image, 0, 0);
-var imgData = imgCanvas.getImageData(0,0,image.width,image.height);
+var imgData = imgCanvas.getImageData(0, 0, image.width, image.height);
 var pixels = imgData.data;
-
-
-
 //get the rgb-values of the pixels and calculate the distance from the correct values
-for(var i=0; i<pixels.length; i+=4) {
+for (var i = 0; i < pixels.length; i += 4) {
     var red = pixels[i];
-    var green = pixels[i+1];
-    var blue = pixels[i+2];
-    var alpha = pixels[i+3];
-  }
-
-
-
-
+    var green = pixels[i + 1];
+    var blue = pixels[i + 2];
+    var alpha = pixels[i + 3];
+}
+*/
