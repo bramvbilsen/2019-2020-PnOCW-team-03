97c277da44a052100d1a759c53ce459b2042fa07
MaartenTaylor <36727362+MaartenTaylor@users.noreply.github.com>
2020-02-18 14:55:18 +0100

    #nomergeconflict

--- public/tests/color_detection/colorTesting.ts
+++ public/tests/color_detection/colorTesting.ts
@@ -1,47 +1,50 @@
-import { createCanvas } from "../../scripts/image_processing/screen_detection/screen_detection";
+import {
+    createCanvas,
+    getHSLColorForPixel,
+    getRGBAColorForPixel,
+} from "../../scripts/image_processing/screen_detection/screen_detection";
+import { loadImage } from "../../scripts/util/images";
+import env from "../../env/env";
+import { IHSLColor, IRGBAColor } from "../../scripts/types/Color";
 
-
-/**
- * 
- * @param src The path to the image that gets loaded and from which we want to analyze te correctness of the colors
- */
-/*
-//function for loading in images
-export async function loadImage(src: string): Promise<HTMLImageElement> {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = src;
-        img.onload = () => {
-            resolve(img);
-        };
-        img.onerror = err => {
-            reject(err);
-        };
-    });
+async function defaultImage() {
+    return await loadImage(env.baseUrl + "/images/normal.png");
 }
 
-//declare a variable 
+async function colortest() {
+    const img = await defaultImage();
+    const canvas = createCanvas(img.width, img.height);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
 
-const image = await loadImage(
-        //"http://localhost:3000/images/unicorn.jpeg");
-
-//create a canvas with which we can iterate the pixels
-const imgCanvas = createCanvas(image.width, image.height);
-const imgCtx = imgCanvas.getContext("2d");
-imgCtx.drawImage(image, 0, 0);
-var imgData = imgCanvas.getImageData(0, 0, image.width, image.height);
-var pixels = imgData.data;
-
-
-
-//get the rgb-values of the pixels and calculate the distance from the correct values
-for (var i = 0; i < pixels.length; i += 4) {
-    var red = pixels[i];
-    var green = pixels[i + 1];
-    var blue = pixels[i + 2];
-    var alpha = pixels[i + 3];
+    const nonColoredScreenCtx = <CanvasRenderingContext2D>(
+        canvas.getContext("2d")
+    );
+    const nonColoredScreenPixelData = nonColoredScreenCtx.getImageData(
+            0,
+            0,
+            img.width,
+            img.height
+        ),
+        Pixels = nonColoredScreenPixelData.data; //hier hebben we de pixels
+    let HSLvalues: IHSLColor[] = Array(img.width * img.height).fill({});
+    let RGBvalues: IRGBAColor[] = Array(img.width * img.height).fill({});
+    for (let i = 0; i < img.height; i++) {
+        for (let j = 0; j < img.width; j++) {
+            HSLvalues[i * img.width + j] = getHSLColorForPixel(
+                0,
+                0,
+                img.width,
+                Pixels
+            );
+            RGBvalues[i * img.width + j] = getRGBAColorForPixel(
+                0,
+                0,
+                img.width,
+                Pixels
+            );
+        }
+    }
+    console.log(HSLvalues);
+    console.log(RGBvalues);
 }
-
-
-*/
-
