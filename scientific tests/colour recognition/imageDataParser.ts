a32990c50423600cc42bb27eee0b7422ad22cecc
SebastiaanWouters <sebastiaan.wouters@student.kuleuven.be>
2020-02-20 12:10:09 +0100

    updated parser for imageData

--- scientific tests/colour recognition/imageDataParser.ts
+++ scientific tests/colour recognition/imageDataParser.ts
@@ -1,3 +1,5 @@
+import {IData, reflectionType, lightType} from "./imgData"
+
 /**
  * Parse the metdata from the pictures to easily
  * understandable data
@@ -13,11 +15,61 @@
 
  function dataParse(data: string) {
     let splitted = data.split("_");
-    for (let i = 0; i++; i < 10) {
-        let current = splitted[i];
-        console.log(current);
+    console.log("startd");
+    console.log(splitted);
+    const imgData = <IData> {};
+    imgData.brightness = parseInt(splitted[0]);
+    imgData.phone_id = parseInt(splitted[1]);
+    imgData.percentage_screen = parseInt(splitted[2]);
+    imgData.color_amount = parseInt(splitted[3]);
+    let amount = imgData.color_amount;
+    let counter = 0;
+    imgData.colors = [];
+    while (amount > 0) {
+        imgData.colors.push(parseInt(splitted[4+counter]));
+        imgData.colors.push(parseInt(splitted[4+counter+1]));
+        imgData.colors.push(parseInt(splitted[4+counter+2]));
+        counter += 3;
+        amount -= 1;
+    }
+    let newIndex = 4 + counter;
+    switch (parseInt(splitted[newIndex++])) {  
+        case 0:  
+         imgData.light_type = lightType.NO_LIGHT;  
+         break;  
+        case 1:  
+         imgData.light_type = lightType.ARTIFICIAL_LIGHT; ;  
+         break; 
+        case 2:  
+         imgData.light_type = lightType.NATURAL_LIGHT; ;  
+         break; 
+       }  
+    imgData.light_type = parseInt(splitted[newIndex++]);
+    switch (parseInt(splitted[newIndex++])) {  
+        case 0:  
+         imgData.reflection_type = reflectionType.NO_REFLECTION;  
+         break;  
+        case 1:  
+        imgData.reflection_type = reflectionType.REFLECTION; ;  
+         break; 
+       }  
+    imgData.screen_type = parseInt(splitted[newIndex++]);
+    imgData.identifier = parseInt(splitted[newIndex]);
+
+    console.log("brightness: " + imgData.brightness);
+    console.log("phone_id: " + imgData.phone_id);
+    console.log("percentage_screen: " + imgData.percentage_screen);
+    console.log("color_amount: " + imgData.color_amount);
+    console.log("colors: " + imgData.colors);
+    console.log("reflection_type: " + imgData.reflection_type);
+    console.log("screen_id: " + imgData.screen_type);
+    console.log("identifier: " + imgData.identifier);
+
+    for (let i = 0; i < splitted.length; i++) {
+        //console.log("chillzzz");
+        
     }
  }
 
-
+ dataParse("25_2_80_3_255_255_255_255_255_244_1_1_1_0_0_1_3_11");
 
